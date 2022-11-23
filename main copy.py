1071a59265766c08062792157c59069f6da3180b
Putu Ayu Kurniasari <57127775+ayoe04@users.noreply.github.com>
2022-11-23 14:09:28 +0700

    program

---
+++ main copy.py
@@ -0,0 +1,40 @@
+import MySQLdb
+import pickle
+import numpy as np
+
+pkl_filename = "progkmeans.pkl"
+
+with open(pkl_filename, 'rb') as file:
+
+    pickle_model = pickle.load(file)
+
+# Connect to server
+cnx = MySQLdb.connect(
+     user = "mbkm_enose",
+     password = "pmPshMlorfRhF2stUeaom1tPOGY9tsK4",
+     database = "mbkm_enose_b3")
+
+# Get a cursor
+cur = cnx.cursor()
+
+# Execute a query
+cur.execute("SELECT * FROM tangkap_sensor_1_ppm ORDER BY id desc limit 1")
+
+result = cur.fetchall()
+for row in result:
+    id = row[0]
+    data = row[2:12]
+    data = np.asarray(data)
+    data = data.reshape(1, -1)
+    hasil = pickle_model.predict((data))
+    print(hasil)
+    hasilstr = ''
+    if (hasil==0):
+        hasilstr = 'Udara Bersih'
+    else:
+        hasilstr = 'Udara Kotor'
+    print(id)
+    cur.execute(f"UPDATE tangkap_sensor_1_ppm SET hasil='{hasilstr}' WHERE id = {id}")
+    cnx.commit()
+
+cnx.close()
